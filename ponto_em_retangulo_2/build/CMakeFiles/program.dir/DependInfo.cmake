798cb1d8109aed9c4f0ed1fe5abd98059c10cde7
igorvicentels <igorvicente7@hotmail.com>
2022-04-12 19:01:15 -0300

    lista finalizada

---
+++ ponto_em_retangulo_2/build/CMakeFiles/program.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/igorv/Documents/lp1/lista1/lista-1-igorvicentels/ponto_em_retangulo_2/src/function.cpp" "CMakeFiles/program.dir/src/function.cpp.obj" "gcc" "CMakeFiles/program.dir/src/function.cpp.obj.d"
+  "C:/Users/igorv/Documents/lp1/lista1/lista-1-igorvicentels/ponto_em_retangulo_2/src/main.cpp" "CMakeFiles/program.dir/src/main.cpp.obj" "gcc" "CMakeFiles/program.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
