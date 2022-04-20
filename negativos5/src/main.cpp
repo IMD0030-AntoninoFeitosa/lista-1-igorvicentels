f60dcb9f7463110be28d585fe046edb03624fe57
IGOR SILVA <68401005+igorvicentels@users.noreply.github.com>
2022-04-20 00:39:29 +0000

    Atualizei negativos5

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -6,16 +6,15 @@ const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
-    int values[SIZE];
+    int value;
     int count = 0;
 
-    for (int i = 0; i < 5; i++) {
-        std::cin >> values[i];
-        if (values[i] < 0) {
+    for (int i = 0; i < SIZE; i++) {
+        cin >> value;
+        if (value < 0) {
             ++count;
         }
     }
-    std::cout << count << std::endl;
+    cout << count << std::endl;
     return 0;
     }
